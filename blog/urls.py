0152cc409fc65ff06a6b3815e082de97b9470dc6
Terrence Amponsah <tamponsah9909@gmail.com>
2019-08-26 00:00:48 -0700

    finished post-editing views, and urls

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('post/<int:pk>', views.PostDetailView.as_view(), name="post-detail"),
     path('blogger/<int:pk>', views.BloggerDetailView.as_view(), name="blogger-detail"),
     path('posts/new_post/', views.new_post, name="new_post"),
+    path('post/<int:pk>/edit/', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -49,3 +49,17 @@ def new_post(request):
     else:
         form = NewPostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = NewPostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = curr_time()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = NewPostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
